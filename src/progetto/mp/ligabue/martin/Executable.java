0212f7e25a5b3c5584cd379e923412e2179fba1d
Martin Ligabue <martinligabue@gmail.com>
2021-11-25 15:40:31 +0100

    rifacciamo il builder, yeah

--- src/progetto/mp/ligabue/martin/Executable.java
+++ src/progetto/mp/ligabue/martin/Executable.java
@@ -2,48 +2,28 @@ package progetto.mp.ligabue.martin;
 
 public class Executable {
     private String name;
-    private boolean isGame= false;
+    private boolean isGame = false;
     private boolean ACO = false;
     private boolean DLSS = false;
     private boolean FSR = false;
-    //make a builder pattern
-    public Executable(String name) {
-        this.name=name;
-    }
-    public static class Builder{
-        private String name;
-        private boolean isGame;
-        private boolean ACO;
-        private boolean DLSS;
-        private boolean FSR;
-        public Builder(String name){
-            this.name = name;
-        }
-        public Builder isGame(boolean isGame){
-            this.isGame = isGame;
-            return this;
-        }
-        public Builder ACO(boolean ACO){
-            this.ACO = ACO;
-            return this;
-        }
-        public Builder DLSS(boolean DLSS){
-            this.DLSS = DLSS;
-            return this;
-        }
-        public Builder FSR(boolean FSR){
-            this.FSR = FSR;
-            return this;
-        }
-        public Executable build(){
-            return new Executable(name);
-        }
-        
-    }
-    public String getName() {
-        return null;
-    } 
 
+    private Executable(String name, boolean isGame, boolean ACO, boolean DLSS, boolean FSR) {
+        this.name = name;
+        this.isGame = isGame;
+        this.ACO = ACO;
+        this.DLSS = DLSS;
+        this.FSR = FSR;
+    }
 
+    // nyeh
+    public Executable withName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    public Executable withIsGame(boolean isGame) {
+        this.isGame = isGame;
+        return this;
+    }// sì, builder
 
 }
