c7b448ef3994cf5c1d923fa865f36f1da01d559f
Martin Ligabue <martinligabue@gmail.com>
2021-11-29 12:02:28 +0100

    Update LogAdapter.java

--- src/progetto/mp/ligabue/martin/LogAdapter.java
+++ src/progetto/mp/ligabue/martin/LogAdapter.java
@@ -1,18 +1,38 @@
 package progetto.mp.ligabue.martin;
 
-public class LogAdapter extends RunLogger {
+import java.lang.System.Logger;
+import java.util.ResourceBundle;
+
+public class LogAdapter extends RunLogger implements Logger {
     public LogAdapter(String name) {
         super(name);
     }
-    
+
     public void start(String name) {
         super.info("Start");
     }
-    
+
     public void stop(String name) {
         super.reset();
     }
-}
 
-    
+    @Override
+    public String getName() {
+        return super.getName();
+    }
+
+    @Override
+    public boolean isLoggable(Level arg0) {
+        return super.isLoggable(arg0);
+    }
+
+    @Override
+    public void log(Level arg0, ResourceBundle arg1, String arg2, Throwable arg3) {
+        super.log(arg0, arg1, arg2, arg3);
+    }
+
+    @Override
+    public void log(Level arg0, ResourceBundle arg1, String arg2, Object... arg3) {
+        super.log(arg0, arg1, arg2, arg3);
+    }
 }
