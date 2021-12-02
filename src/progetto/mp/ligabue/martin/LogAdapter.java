80e3301f364fd6cd3562b0a5d4971a88e275aba1
Martin Ligabue <martinligabue@gmail.com>
2021-12-02 16:03:39 +0100

    pulizia, commenti, img

--- src/progetto/mp/ligabue/martin/LogAdapter.java
+++ src/progetto/mp/ligabue/martin/LogAdapter.java
@@ -4,35 +4,5 @@ import java.lang.System.Logger;
 import java.util.ResourceBundle;
 
 public class LogAdapter extends RunLogger implements Logger {
-    public LogAdapter(String name) {
-        super(name);
-    }
-
-    public void start(String name) {
-        super.info("Start");
-    }
-
-    public void stop(String name) {
-        super.reset();
-    }
-
-    @Override
-    public String getName() {
-        return super.getName();
-    }
-
-    @Override
-    public boolean isLoggable(Level arg0) {
-        return super.isLoggable(arg0);
-    }
-
-    @Override
-    public void log(Level arg0, ResourceBundle arg1, String arg2, Throwable arg3) {
-        super.log(arg0, arg1, arg2, arg3);
-    }
-
-    @Override
-    public void log(Level arg0, ResourceBundle arg1, String arg2, Object... arg3) {
-        super.log(arg0, arg1, arg2, arg3);
-    }
+    
 }
--- src/progetto/mp/ligabue/martin/RunLogger.java
+++ src/progetto/mp/ligabue/martin/RunLogger.java
@@ -1,13 +1,11 @@
 package progetto.mp.ligabue.martin;
 
-import java.util.logging.LogManager;
-
 public class RunLogger {
-    //make methods Start Stop and Read  
-    public void start(String name) {
-        LogManager.getLogManager().getLogger(name).info("Start");
-    }
-    public void stop(String name) {
-        LogManager.getLogManager().reset();
-    }
+	public void start(String name) {
+		System.out.println(name);//altamente rotta come cosa
+	}
+
+	public void stop(String name) {
+		//nothing to stop
+	}
 }
