f1a5c5ed83f0311a37d54a75ffe0f34e5caa1daf
Martin Ligabue <martinligabue@gmail.com>
2021-12-07 15:50:53 +0100

    Revert "wtf am i doing"
    This reverts commit f4232f2d46ef460304bf961c8bf0878c749429a3.
    
    Revert "set?"
    
    This reverts commit c9b3f155465afbe3890309235cb56207906ba4f2.

--- tests/progetto/mp/ligabue/martin/Library.java
+++ tests/progetto/mp/ligabue/martin/Library.java
@@ -4,15 +4,14 @@ import java.util.*;
 
 public class Library implements Iterable<Executable> {
 	private List<Executable> exe = new ArrayList<>();
-	private Set<Executable> exeSet = new HashSet<>();
+
 	// this willl have a collection of Executables(?)
 	public boolean hasNext() {
-		return exe.isEmpty()||exeSet.isEmpty();///aaaaAAAAAAAAAAAAAAAA
+		return exe.isEmpty();
 	}
 
 	@Override
 	public java.util.Iterator<Executable> iterator() {
 		return exe.iterator();
 	}
-	
 }
