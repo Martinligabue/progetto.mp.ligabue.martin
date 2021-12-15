ce0b5f53e0807243143eb565069139df79c5c83f
Martin Ligabue <martinligabue@gmail.com>
2021-12-15 15:15:20 +0100

    adiòs

--- src/progetto/mp/ligabue/martin/Execute.java
+++ src/progetto/mp/ligabue/martin/Execute.java
@@ -2,5 +2,17 @@ package progetto.mp.ligabue.martin;
 
 public class Execute {
 	// take a runner, a game, start the logger, manage excemxptions
-
+	public void run(Executable exe) {
+		//choose a Runner
+		Runner runner = new Runner();//???
+		
+		//take the exe
+		//start the logger
+		LoggerAdapter a = new LoggerAdapter();
+		a.start();
+		
+		//run the exe
+		exe.run();
+		//coninuously check for errors
+	}
 }
